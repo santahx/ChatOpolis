8da4176299ccb218824d18f427db17065ad857f1
santa <santahydroix@gmail.com>
2023-11-03 20:41:39 +0100

    1.0 License Set up

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,23 @@
+//Copyright (C) 2020  Christian Pauly
+
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU Affero General Public License as published
+//by the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU Affero General Public License for more details.
+
+//You should have received a copy of the GNU Affero General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+//santahx@gmail.com ís used for issue contact on this resporitory.
+//this project is just re-used as a base for a new chat-app.
+//the Licence will be continued by letting the original in the Project.
+
+
 import 'package:flutter/material.dart';
 
 import 'package:collection/collection.dart';
